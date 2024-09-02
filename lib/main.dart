7097523df8151aa1a0516b0b5d59e9f91d874ff0
Mahfuz-00 <sayembinabdullah@gmail.com>
2024-09-02 17:23:14 +0600

    Last Updated: 2nd Sep 2024

--- lib/main.dart
+++ lib/main.dart
@@ -13,13 +13,28 @@ import 'UI/Bloc/second_page_cubit.dart';
 import 'UI/Bloc/third_page_cubit.dart';
 import 'UI/Pages/Splashscreen UI/splashscreenUI.dart';
 
+/// The entry point of the Flutter application.
+///
+/// This function initializes necessary services, checks and clears the cache,
+/// and then runs the main application widget.
+///
+/// Actions:
+/// - Calls [checkAndClearCache] to ensure any old cache is cleared
+///   when the app version changes.
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await checkAndClearCache();
   runApp(const MyApp());
 }
 
-/// Checks the app version and clears the cache if the app has been updated.
+/// Checks the current application version against the stored version
+/// in [SharedPreferences]. If the versions differ, it clears the cache
+/// using [DefaultCacheManager] and updates the stored version.
+///
+/// - [prefs]: Instance of [SharedPreferences] used to access stored data.
+/// - [packageInfo]: Instance of [PackageInfo] that holds version information.
+/// - [currentVersion]: A string representing the current version of the app.
+/// - [storedVersion]: The previously stored version of the app, retrieved from [SharedPreferences].
 Future<void> checkAndClearCache() async {
   SharedPreferences prefs = await SharedPreferences.getInstance();
   PackageInfo packageInfo = await PackageInfo.fromPlatform();
@@ -32,15 +47,31 @@ Future<void> checkAndClearCache() async {
   }
 }
 
+/// A stateless widget that represents the main application.
+///
+/// This widget sets the system UI overlay style and initializes the
+/// necessary [Bloc] providers for state management.
+///
+/// - [AuthCubit]: Bloc provider for managing authentication state.
+/// - [EmailCubit]: Bloc provider for managing email-related state.
+/// - [FirstPageCubit]: Bloc provider for managing first page state.
+/// - [SecondPageCubit]: Bloc provider for managing second page state.
+/// - [ThirdPageCubit]: Bloc provider for managing third page state.
+/// - [CombinedDataCubit]: Bloc provider that combines data from the first,
+///   second, and third page cubits.
+///
+/// Actions:
+/// - Sets the system UI overlay style using [SystemChrome].
+/// - Configures the application with a [MaterialApp] that includes
+///   theme and home page settings.
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
       statusBarColor: Color.fromRGBO(
-          0, 162, 222, 1), // Change the status bar color here
+          0, 162, 222, 1),
     ));
     return MultiBlocProvider(
       providers: [
@@ -51,12 +82,9 @@ class MyApp extends StatelessWidget {
         BlocProvider(create: (context) => ThirdPageCubit()),
         BlocProvider(
           create: (context) {
-            // Get instances of the required Cubits
             final firstPageCubit = context.read<FirstPageCubit>();
             final secondPageCubit = context.read<SecondPageCubit>();
             final thirdPageCubit = context.read<ThirdPageCubit>();
-
-            // Pass the instances to CombinedDataCubit
             return CombinedDataCubit(
               firstPageCubit: firstPageCubit,
               secondPageCubit: secondPageCubit,
